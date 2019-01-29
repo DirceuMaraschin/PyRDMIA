e3e713c69cfc617eec0eccc50c9f245a1c70f2ae
Dirceu Maraschin Jr <dirceu_maraschin@hotmail.com>
2019-01-29 01:30:26 -0200

    adjusting methods for rdm notation

--- src/utils/QuantitativeMetrics.py
+++ src/utils/QuantitativeMetrics.py
@@ -50,31 +50,35 @@ class QuantitativeMetrics(object):
 	@staticmethod
 	def fullSpan(intervalList):
 		fspan = rdmia.number(0.0)
-		lowers = []
-		uppers = []
+		#lowers = []
+		#uppers = []
+		item = []
 
 		for i in range(len(intervalList)):
-			lowers.append(0)
-			uppers.append(0)
-			lowers[i] = intervalList[i].lower()
-			uppers[i] = intervalList[i].upper()
+			#lowers.append(0)
+			#uppers.append(0)
+			item.append(0)
+			#lowers[i] = intervalList[i].lower()
+			#uppers[i] = intervalList[i].upper()
+			item[i] = intervalList[i]
 
-		lowers.sort()
-		uppers.sort()
+		#lowers.sort()
+		#uppers.sort()
+		item.sort()
 
-		if(intervalList[len(intervalList)-1].lower() > intervalList[0].upper()):
-			upper = uppers[len(uppers)-1] - uppers[0]
-			lower = lowers[len(lowers)-1] - lowers[0]
-			fspan = rdmia.number(lower,upper)
-		else:
-			upper = lowers[len(intervalList)-1] - lowers[0]
-			fspan = rdmia.number(0, upper)
+		#if(intervalList[len(intervalList)-1].lower() > intervalList[0].upper()):
+		#	upper = uppers[len(uppers)-1] - uppers[0]
+		#	lower = lowers[len(lowers)-1] - lowers[0]
+		#	fspan = rdmia.number(lower,upper)
+		#else:
+		#	upper = lowers[len(intervalList)-1] - lowers[0]
+		#	fspan = rdmia.number(0, upper)
+		fspan = item[len(intervalList)-1] - item[0]
 		return fspan
 
 	#The variance operation is used the average operation of that class, getting a list of inervalares values.
 	@staticmethod
 	def variance(intervalList):
-		variance = rdmia.number(0.0)
 		variance = rdmia.number(0.0)
 		for i in range(len(intervalList)):
 			variance += (intervalList[i] - QuantitativeMetrics.average(intervalList))**2.0
@@ -107,13 +111,16 @@ class QuantitativeMetrics(object):
 			n = len(intervalListTwo)
 
 		for i in range(n):
-			productXlow = intervalListOne[i].lower() - AverageX.upper()
-			productXupp = intervalListOne[i].upper() - AverageX.lower()
-			productYlow = intervalListTwo[i].lower() - AverageY.upper()
-			productYupp = intervalListTwo[i].upper() - AverageY.lower()
-			X = rdmia.number(productXlow,productXupp)
-			Y = rdmia.number(productYlow,productYupp)
-			coVariance+=X*Y
+			#productXlow = intervalListOne[i].lower() - AverageX.upper()
+			productX = intervalListOne[i] - AverageX
+			#productXupp = intervalListOne[i].upper() - AverageX.lower()
+			#productYlow = intervalListTwo[i].lower() - AverageY.upper()
+			productY = intervalListTwo[i] - AverageY
+			#productYupp = intervalListTwo[i].upper() - AverageY.lower()
+			#X = rdmia.number(productXlow,productXupp)
+			#Y = rdmia.number(productYlow,productYupp)
+			#coVariance+=X*Y
+			coVariance+=productX * productY
 		return coVariance/(n)
 
 	#This method contains dependence of the correlation coefficient used when needed to determine the correlation between two sets of data
